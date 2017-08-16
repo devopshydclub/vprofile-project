12f73e6e3acf8dedf5ccc15a3b4f57586dab6e18
Wahid Khan <wahid@gmail.com>
2017-08-16 14:18:43 +0530

    Improving checkstyel warnings for model

--- src/main/java/com/visualpathit/account/model/User.java
+++ src/main/java/com/visualpathit/account/model/User.java
@@ -7,15 +7,21 @@ import java.util.Set;
 @Entity
 @Table(name = "user")
 public class User {
+	/** the id field !*/
     private Long id;
+    /** the user name field !*/
     private String username;
+    /** the id field !*/
     private String password;
+    /** the password field !*/
     private String userEmail;
+    /** the passwordConfirm field !*/
     private String passwordConfirm;
+    /** the roles field !*/
     private Set<Role> roles;
     /**
      * {@inheritDoc}} 
-     */
+     !*/
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     /**
@@ -26,66 +32,66 @@ public class User {
     }
     /**
      * {@inheritDoc}} 
-     */
+     !*/
     public final void setId(final Long id) {
         this.id = id;
     }
     /**
      * {@inheritDoc}} 
-     */
+     !*/
     public String getUsername() {
         return username;
     }
     /**
      * {@inheritDoc}} 
-     */
+     !*/
     public final void setUsername(final String username) {
         this.username = username;
     }
     /**
      * {@link User#password}
-     */
+     !*/
     public String getPassword() {
         return password;
     }
     /**
      * {@inheritDoc}} 
-     */
+     !*/
     public final void setPassword(final String password) {
         this.password = password;
     }
     /**
      * {@link User#userEmail}
-     */
+     !*/
     public String getUserEmail() {
 		return userEmail;
 	}
     /**
      * {@inheritDoc}} 
-     */
+     !*/
 	public final void setUserEmail(final String userEmail) {
 		this.userEmail = userEmail;
 	}
 
 	 /**
      * {@inheritDoc}} 
-     */
+     !*/
 	@Transient
 	/**
      * {@link User#passwordConfirm}
-     */
+     !*/
     public String getPasswordConfirm() {
         return passwordConfirm;
     }
 	 /**
      * {@inheritDoc}} 
-     */
+     !*/
     public final void setPasswordConfirm(final String passwordConfirm) {
         this.passwordConfirm = passwordConfirm;
     }
     /**
      * {@inheritDoc}} 
-     */
+     !*/
     @ManyToMany
     @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     public Set<Role> getRoles() {
@@ -93,7 +99,7 @@ public class User {
     }
     /**
      * {@inheritDoc}} 
-     */
+     !*/
     public final void setRoles( final Set<Role> roles) {
         this.roles = roles;
     }
