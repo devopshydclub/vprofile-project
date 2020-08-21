96b36e3a934e3d8d8bea8f056784396608d14f91
ImranT <devopshydclub@gmail.com>
2020-08-21 09:30:24 +0530

    updated vprofile url

--- src/test/java/DevOPS/devOPS/VProfile_TestSuite.java
+++ src/test/java/DevOPS/devOPS/VProfile_TestSuite.java
@@ -18,7 +18,7 @@ public class VProfile_TestSuite {
 		
 		WebDriver driver = baseDriver.Chromedriver();
 		vprofileHomePagePO vpo = new vprofileHomePagePO(driver);
-		vpo.loginPage("http://192.168.33.12:8080/login");
+		vpo.loginPage("http://54.90.50.239:8080/vprofile/login");
 		vpo.loginAction("admin_vp", "admin_vp");
 		driver.close();
 	    
