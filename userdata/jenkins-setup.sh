48da7615c7b15cd120659507604c0e0f632df4cf
Imran Teli <Imran Teli>
2020-09-30 11:25:39 +0530

    scripts for ci servers

---
+++ userdata/jenkins-setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt update
+sudo apt install openjdk-8-jdk -y
+sudo apt install maven git wget unzip -y
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install jenkins -y
