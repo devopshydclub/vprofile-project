73969cf3eb6b504ebd53a2b5c64e9dbe57f5bce8
imran <imran@gmail.com>
2020-09-30 15:27:05 +0530

    Testing autobuild trigger in jenkins

--- userdata/jenkins-setup.sh
+++ userdata/jenkins-setup.sh
@@ -6,3 +6,4 @@ wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key 
 sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 sudo apt-get install jenkins -y
+###
