6ae70599e9e318d1a6f639636320f28143967ee2
imranvisualpath <72437104+imranvisualpath@users.noreply.github.com>
2020-10-21 18:48:40 +0530

    Doc

--- src/main/java/com/visualpathit/account/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.visualpathit.account.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.visualpathit.account.model.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Long>{
-}
