dc90f585263c95d892630d7e49b592a8fb3ed036
imranvisualpath <72437104+imranvisualpath@users.noreply.github.com>
2020-11-07 22:47:13 +0530

    Ansible Vpro stack code

--- src/main/java/com/visualpathit/account/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.visualpathit.account.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.visualpathit.account.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
--- src/main/java/com/visualpathit/account/service/SecurityService.java
+++
@@ -1,9 +0,0 @@
-package com.visualpathit.account.service;
-
-/** method for finding already added user !*/
-public interface SecurityService {
-	/** {@inheritDoc}} !*/
-    String findLoggedInUsername();
-
-    void autologin(String username, String password);
-}
