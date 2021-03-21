5ae3bd3fafcf336cabf9a1f50802659262c90668
devopshydclub <46835275+devopshydclub@users.noreply.github.com>
2021-03-21 10:29:56 +0530

    Updated RMQ setup with latest version on centos 7

--- userdata/rabbitmq.sh
+++ userdata/rabbitmq.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-sudo yum update -y
 sudo yum install epel-release -y
-sudo yum install socat -y
-sudo yum install erlang -y
+sudo yum update -y
 sudo yum install wget -y
 cd /tmp/
-wget https://www.rabbitmq.com/releases/rabbitmq-server/v3.6.10/rabbitmq-server-3.6.10-1.el7.noarch.rpm
-sudo rpm --import https://www.rabbitmq.com/rabbitmq-release-signing-key.asc
-sudo rpm -Uvh /tmp/rabbitmq-server-3.6.10-1.el7.noarch.rpm
+wget http://packages.erlang-solutions.com/erlang-solutions-1.0-1.noarch.rpm
+sudo rpm -Uvh erlang-solutions-1.0-1.noarch.rpm
+sudo yum -y install erlang socat logrotate
+wget https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.8.8/rabbitmq-server-3.8.8-1.el6.noarch.rpm
+sudo rpm --import https://www.rabbitmq.com/rabbitmq-signing-key-public.asc
+sudo rpm -Uvh rabbitmq-server-3.8.8-1.el6.noarch.rpm
 sudo systemctl start rabbitmq-server
 sudo systemctl enable rabbitmq-server
 sudo systemctl status rabbitmq-server
