78fd26d19da235b704ea3f841ab225ec21d42d76
devopshydclub <46835275+devopshydclub@users.noreply.github.com>
2021-05-22 13:13:44 +0530

    Updated erlang and rabbitmq version compatibility

--- vagrant/Automated_provisioning/rabbitmq.sh
+++ vagrant/Automated_provisioning/rabbitmq.sh
@@ -3,12 +3,12 @@ sudo yum install epel-release -y
 sudo yum update -y
 sudo yum install wget -y
 cd /tmp/
-wget http://packages.erlang-solutions.com/erlang-solutions-1.0-1.noarch.rpm
-sudo rpm -Uvh erlang-solutions-1.0-1.noarch.rpm
-sudo yum -y install erlang socat logrotate
-wget https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.8.8/rabbitmq-server-3.8.8-1.el6.noarch.rpm
-sudo rpm --import https://www.rabbitmq.com/rabbitmq-signing-key-public.asc
-sudo rpm -Uvh rabbitmq-server-3.8.8-1.el6.noarch.rpm
+wget http://packages.erlang-solutions.com/erlang-solutions-2.0-1.noarch.rpm
+sudo rpm -Uvh erlang-solutions-2.0-1.noarch.rpm
+sudo yum -y install erlang
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.
+sh | sudo bash
+yum install rabbitmq-server -y
 sudo systemctl start rabbitmq-server
 sudo systemctl enable rabbitmq-server
 sudo systemctl status rabbitmq-server
