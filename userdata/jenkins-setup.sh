ec33ab8dd2deb6a8b936f97eb4887646c4af1381
devopshydclub <46835275+devopshydclub@users.noreply.github.com>
2021-10-05 06:39:10 +0530

    Fixed, Certificate verification failed error.

--- userdata/jenkins-setup.sh
+++ userdata/jenkins-setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo apt update
 sudo apt install openjdk-8-jdk -y
+sudo apt install ca-certificates -y
 sudo apt install maven git wget unzip -y
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
