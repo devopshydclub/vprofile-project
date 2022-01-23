fcb0a5e28d276af94220da5b88187a246c6ff422
devopshydclub <46835275+devopshydclub@users.noreply.github.com>
2022-01-23 10:33:54 +0530

    Jenkins Setup Script updated with new steps

--- userdata/jenkins-setup.sh
+++ userdata/jenkins-setup.sh
@@ -3,8 +3,11 @@ sudo apt update
 sudo apt install openjdk-8-jdk -y
 sudo apt install ca-certificates -y
 sudo apt install maven git wget unzip -y
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
 sudo apt-get install jenkins -y
 ###
