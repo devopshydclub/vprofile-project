bdd5b4da95378ba75b665d2a6ac6a641dd243389
chnze33 <chnze33@gmail.com>
2022-03-30 22:40:04 -0500

    Space

--- userdata/nexus-setup.sh
+++ userdata/nexus-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-yum install java-1.8.0-openjdk.x86_64 wget -y   
-mkdir -p /opt/nexus/   
-mkdir -p /tmp/nexus/                           
+yum install java-1.8.0-openjdk.x86_64 wget -y
+mkdir -p /opt/nexus/
+mkdir -p /tmp/nexus/
 cd /tmp/nexus
 NEXUSURL="https://download.sonatype.com/nexus/3/latest-unix.tar.gz"
 wget $NEXUSURL -O nexus.tar.gz
@@ -10,22 +10,22 @@ NEXUSDIR=`echo $EXTOUT | cut -d '/' -f1`
 rm -rf /tmp/nexus/nexus.tar.gz
 rsync -avzh /tmp/nexus/ /opt/nexus/
 useradd nexus
-chown -R nexus.nexus /opt/nexus 
+chown -R nexus.nexus /opt/nexus
 cat <<EOT>> /etc/systemd/system/nexus.service
-[Unit]                                                                          
-Description=nexus service                                                       
-After=network.target                                                            
-                                                                  
-[Service]                                                                       
-Type=forking                                                                    
-LimitNOFILE=65536                                                               
-ExecStart=/opt/nexus/$NEXUSDIR/bin/nexus start                                  
-ExecStop=/opt/nexus/$NEXUSDIR/bin/nexus stop                                    
-User=nexus                                                                      
-Restart=on-abort                                                                
-                                                                  
-[Install]                                                                       
-WantedBy=multi-user.target                                                      
+[Unit]
+Description=nexus service
+After=network.target
+
+[Service]
+Type=forking
+LimitNOFILE=65536
+ExecStart=/opt/nexus/$NEXUSDIR/bin/nexus start
+ExecStop=/opt/nexus/$NEXUSDIR/bin/nexus stop
+User=nexus
+Restart=on-abort
+
+[Install]
+WantedBy=multi-user.target
 
 EOT
 
