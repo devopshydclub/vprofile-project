b13d641dd08c2550c69e83eb571f5c670d8bf908
devopshydclub <46835275+devopshydclub@users.noreply.github.com>
2022-09-10 11:39:16 +0530

    Updated jdk11 version

--- userdata/jenkins-setup.sh
+++ userdata/jenkins-setup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 sudo apt update
-sudo apt install openjdk-8-jdk -y
-sudo apt install ca-certificates -y
-sudo apt install maven git wget unzip -y
+sudo apt install openjdk-11-jdk -y
+sudo apt install maven -y
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
