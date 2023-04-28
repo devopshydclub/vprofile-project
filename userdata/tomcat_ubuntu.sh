7962ebe3f4cf4e7a72d26edcefbc7e310893bfb7
devopshydclub <46835275+devopshydclub@users.noreply.github.com>
2023-04-28 10:13:15 +0530

    updated version to tomcat9 for ubuntu20

--- userdata/tomcat_ubuntu.sh
+++ userdata/tomcat_ubuntu.sh
@@ -2,4 +2,4 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install openjdk-8-jdk -y
-sudo apt install tomcat8 tomcat8-admin tomcat8-docs tomcat8-common git -y
+sudo apt install tomcat9 tomcat9-admin tomcat9-docs tomcat9-common git -y
