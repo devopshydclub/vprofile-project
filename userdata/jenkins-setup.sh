402b02b4cc89131208bfd0bf0d8eae49e46afb3f
devopshydclub <46835275+devopshydclub@users.noreply.github.com>
2023-05-13 14:35:47 +0530

    Update jenkins-setup.sh

--- userdata/jenkins-setup.sh
+++ userdata/jenkins-setup.sh
@@ -2,11 +2,13 @@
 sudo apt update
 sudo apt install openjdk-11-jdk -y
 sudo apt install maven -y
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+  
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
+
 sudo apt-get update
 sudo apt-get install jenkins -y
 ###
