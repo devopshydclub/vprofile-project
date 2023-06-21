bcd652b632633e58034f4f9b81828277507f8817
Imran Teli <devopshydclub@gmail.com>
2023-06-21 05:47:03 +0530

    sql update

--- vagrant/Automated_provisioning_WinMacIntel/mysql.sh
+++ vagrant/Automated_provisioning_WinMacIntel/mysql.sh
@@ -10,7 +10,7 @@ sudo yum install mariadb-server -y
 sudo systemctl start mariadb
 sudo systemctl enable mariadb
 cd /tmp/
-git clone -b local-setup https://github.com/devopshydclub/vprofile-project.git
+git clone -b main https://github.com/devopshydclub/vprofile-project.git
 #restore the dump file for the application
 sudo mysqladmin -u root password "$DATABASE_PASS"
 sudo mysql -u root -p"$DATABASE_PASS" -e "UPDATE mysql.user SET Password=PASSWORD('$DATABASE_PASS') WHERE User='root'"
