76712be4e285e09da9839ccb0e804721914b67fd
devopshydclub <46835275+devopshydclub@users.noreply.github.com>
2023-06-21 14:36:43 +0530

    Update mysql.sh

--- vagrant/Automated_provisioning_MacOSM1/mysql.sh
+++ vagrant/Automated_provisioning_MacOSM1/mysql.sh
@@ -17,7 +17,7 @@ cd /tmp/
 git clone -b main https://github.com/devopshydclub/vprofile-project.git
 #restore the dump file for the application
 sudo mysqladmin -u root password "$DATABASE_PASS"
-sudo mysql -u root -p"$DATABASE_PASS" -e "UPDATE mysql.user SET Password=PASSWORD('$DATABASE_PASS') WHERE User='root'"
+#sudo mysql -u root -p"$DATABASE_PASS" -e "UPDATE mysql.user SET Password=PASSWORD('$DATABASE_PASS') WHERE User='root'"
 sudo mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')"
 sudo mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User=''"
 sudo mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%'"
