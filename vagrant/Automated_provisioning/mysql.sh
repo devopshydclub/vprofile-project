1f7437ffecc7c7e61cbe35a8f8187fc20f11359f
devopshydclub <46835275+devopshydclub@users.noreply.github.com>
2023-09-01 23:24:26 +0400

    Add files via upload

--- vagrant/Automated_provisioning/mysql.sh
+++ vagrant/Automated_provisioning/mysql.sh
@@ -10,10 +10,9 @@ sudo yum install mariadb-server -y
 sudo systemctl start mariadb
 sudo systemctl enable mariadb
 cd /tmp/
-git clone -b local-setup https://github.com/devopshydclub/vprofile-project.git
+git clone -b main https://github.com/hkhcoder/vprofile-project.git
 #restore the dump file for the application
 sudo mysqladmin -u root password "$DATABASE_PASS"
-sudo mysql -u root -p"$DATABASE_PASS" -e "UPDATE mysql.user SET Password=PASSWORD('$DATABASE_PASS') WHERE User='root'"
 sudo mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')"
 sudo mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User=''"
 sudo mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%'"
--- vagrant/Automated_provisioning/tomcat_ubuntu.sh
+++ vagrant/Automated_provisioning/tomcat_ubuntu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt update
 sudo apt upgrade -y
-sudo apt install openjdk-11-jdk -y
-sudo apt install tomcat9 tomcat9-admin tomcat9-docs tomcat9-common git -y
+sudo apt install openjdk-8-jdk -y
+sudo apt install tomcat8 tomcat8-admin tomcat8-docs tomcat8-common git -y
