64ab53563461aa67c29d172f6cf8c4d803ef13c6
NkengBlaise <63776541+NkengBlaise@users.noreply.github.com>
2025-01-14 09:14:11 +0100

    Update jenkins-setup.sh

--- userdata/jenkins-setup.sh
+++ userdata/jenkins-setup.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 sudo apt update
 sudo apt install openjdk-11-jdk -y
+sudo apt install fontconfig openjdk-17-jre -y
 sudo apt install maven -y
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-  
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io.key | sudo tee \
+/usr/share/keyrings/jenkins-keyring.asc > /dev/null
 
-sudo apt-get update
-sudo apt-get install jenkins -y
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+https://pkg.jenkins.io/debian binary/ | sudo tee \
+/etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt update
+sudo apt install jenkins -y
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+
 ###
